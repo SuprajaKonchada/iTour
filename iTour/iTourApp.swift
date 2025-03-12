f9ca982b7f27c092b2d092dc27a261bc8c86b12f
SuprajaKonchada <supraja.konchada@cofgnine.com>
2025-03-12 14:29:21 +0800

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by user262000 on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
